ca45b978e23a568500ce7fae1d21b06a1dfc1d6e
U-DESKTOP-6E768SP\whois <whoisroseman@gmail.com>
2017-10-04 14:27:50 -0400

    Started basic symbol detection and added display function for symbol stack.
    dfsadf
    #

--- PLC_Project1/PLCProject1.cpp
+++ PLC_Project1/PLCProject1.cpp
@@ -11,7 +11,9 @@
 #include <regex>
 
 
+
 using namespace std;
+void display_symbols(stack<char> symbols);
 int main()
 {
 	regex var_start_rgx("[^\\d*");		// Don't want variables to start with numbers so we will invert this in our conditional
@@ -19,22 +21,31 @@ int main()
 	regex symb_rgx("[:,;(){}]");
 	regex op_rgx("[+*/-]");
 
-	stack<string> variables;			// Can't start with a digit. 
-	stack<int> constants;				// Must only contain digits/
-	stack<char> symbols;				// : , ; () {}
-	stack<char> operators;				// + - * /
-	stack<string> keywords;
-	stack<string> errors;
+	stack<string> variables;			// Can't start with a digit.  Andrew
+	stack<int> constants;				// Must only contain digits/  Brett
+	stack<char> symbols;				// : , ; () {} Andrew
+	stack<char> operators;				// + - * / =  Brett
+	stack<string> keywords;				// Andrew
+	stack<string> comments;				// we need to look for/* */ ~good luck
+	stack<string> errors;				// both(basically just an else to catch whatever doesn't belong)
 
 	ifstream file ("code.txt");
 	string line;
 	
 
 	if (file.is_open()) {
-		
 
+//	Loop through file line by line and analyze as we go.
 		while (getline(file, line)) {
-			cout << line << "\n";
+			
+			// iterate across the line to look for symbols
+			for (int i = 0; i < line.length(); i++) {
+
+				if (regex_match(line, symb_rgx)) {
+					symbols.push(line[i]);		// if the symbol is valid add it to our symbol stack
+				}
+			}
+		
 
 		}
 
@@ -43,10 +54,20 @@ int main()
 	else {
 		cout << "[!] Unable to open file\n";
 	}
+	display_symbols(symbols);
+
 	system("pause");
     return 0;
 }
 
+void display_symbols(stack<char> symbols) {
+	cout << "Symbols found: ";
+	while (!symbols.empty()) {
+		cout << symbols.top();
+		symbols.pop();
+	}
+
+}
 
 
 
